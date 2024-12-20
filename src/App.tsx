fa72fa3e373ee7b02ad755e9c90bb306f49deb6e
chandanagayathrig2003 <chandanagayathrig@gmail.com>
2024-12-20 11:17:57 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CreativeCanvas } from './components/CreativeCanvas';
+
+function App() {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <CreativeCanvas />
+    </div>
+  );
+}
+
+export default App;
